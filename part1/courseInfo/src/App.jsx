b7d3325259eb56770aebd9e4d20cd9a97cc37621
Jagadeesh985 <monangijagadeesh647@gmail.com>
2024-03-23 17:03:21 +0530

    Course info part2

--- part1/courseInfo/src/App.jsx
+++ part1/courseInfo/src/App.jsx
@@ -32,17 +32,11 @@ const Header = (props) => {
 };
 const Content = (props) => {
   return (
-    <>
-      <p>
-        {props.part1} {props.exercises1}
-      </p>
-      <p>
-        {props.part2} {props.exercises2}
-      </p>
-      <p>
-        {props.part3} {props.exercises3}
-      </p>
-    </>
+    <div>
+      <Part part={props.part1} exercises={props.exercises1} />
+      <Part part={props.part2} exercises={props.exercises3} />
+      <Part part={props.part3} exercises={props.exercises3} />
+    </div>
   );
 };
 const Total = (props) => {
@@ -50,6 +44,14 @@ const Total = (props) => {
     <p>
       Number of exercises{" "}
       {props.exercises1 + props.exercises2 + props.exercises3}
+    </p>
+  );
+};
+
+const Part = (props) => {
+  return (
+    <p>
+      {props.part} {props.exercises}
     </p>
   );
 };
