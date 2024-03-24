43d56a673fa13551e56e8b8626ca31fb385a289e
Jagadeesh985 <monangijagadeesh647@gmail.com>
2024-03-24 18:16:00 +0530

    Unicafe final step:Exercise 1.11

--- part1/unicafe/src/App.jsx
+++ part1/unicafe/src/App.jsx
@@ -5,9 +5,7 @@ const App = () => {
   const [good, setGood] = useState(0);
   const [neutral, setNeutral] = useState(0);
   const [bad, setBad] = useState(0);
-  const all = good + neutral + bad;
-  const average = (good * 1 + bad * -1) / all;
-  const positive = (good / all) * 100;
+
   return (
     <div>
       <Header text="give feedback" />
@@ -32,14 +30,11 @@ const App = () => {
         />
       </div>
       <h2>Statistics</h2>
-      <div>
-        <p>good {good}</p>
-        <p>neutral {neutral}</p>
-        <p>Bad {bad}</p>
-        <p>all {all}</p>
-        <p>average {isNaN(average) ? 0 : average}</p>
-        <p>positive {isNaN(positive) ? 0 : positive} %</p>
-      </div>
+      {good > 0 || neutral > 0 || bad > 0 ? (
+        <Statistics good={good} neutral={neutral} bad={bad} />
+      ) : (
+        <p>No feedback given</p>
+      )}
     </div>
   );
 };
@@ -50,6 +45,42 @@ const Header = ({ text }) => {
 
 const Button = ({ text, handler }) => {
   return <button onClick={handler}>{text}</button>;
+};
+
+const Statistics = ({ good, neutral, bad }) => {
+  const all = good + neutral + bad;
+  const average = (good * 1 + bad * -1) / all;
+  const positive = (good / all) * 100;
+  return (
+    <table>
+      <tbody>
+        <tr>
+          <td>good</td>
+          <td>{good}</td>
+        </tr>
+        <tr>
+          <td>neutral</td>
+          <td>{neutral}</td>
+        </tr>
+        <tr>
+          <td>bad</td>
+          <td>{bad}</td>
+        </tr>
+        <tr>
+          <td>all</td>
+          <td>{all}</td>
+        </tr>
+        <tr>
+          <td>average</td>
+          <td>{isNaN(average) ? 0 : average}</td>
+        </tr>
+        <tr>
+          <td>positive</td>
+          <td>{isNaN(positive) ? 0 : positive} %</td>
+        </tr>
+      </tbody>
+    </table>
+  );
 };
 
 export default App;
